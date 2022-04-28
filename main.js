04ee6f9e4af57e73a891ab8596ff975443bfff35
disha080309 <84317794+disha080309@users.noreply.github.com>
2022-04-28 15:05:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function preload(){
+
+}
+function setup(){
+canvas=createCanvas(300,300);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+posenet=ml5.poseNet(video,modelLoaded);
+posenet.on('pose',gotPoses);
+}
+function gotPoses(results){
+    if (results.length>0)   
+    {
+        console.log(results);
+        console.log("nose X position "+results[0].pose.nose.x);
+        console.log("Nose Y Position "+results[0].pose.nose.y);
+        console.log("Mouth X position "+results[0].pose.nose.x);
+        console.log("Mouth Y Position "+results[0].pose.nose.y+10);
+        console.log("left eye X position "+results[0].pose.leftEye.x);
+        console.log("left eye Y Position "+results[0].pose.leftEye.y);
+    
+    } 
+}
+function modelLoaded(){
+    console.log("model Has Loaded")
+}
+function take_snapshot(){
+    save("my_filter_selfie.png");
+}
+function draw(){
+    image(video,0,0,300,300);
+}
+
